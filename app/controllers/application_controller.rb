8f7624e2a607a11a416ef12488cecf5292d862fa
sou1218 <0un324410090r7v@ezweb.ne.jp>
2021-08-03 11:47:08 +0900

    deviseの導入、新規登録・ログイン機能の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
   
 
   private
@@ -10,6 +10,8 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
-
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :family_name_kana, :first_name, :first_name_kana, :birthday])
+  end
   
 end
