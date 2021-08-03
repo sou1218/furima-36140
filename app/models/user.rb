7c760d97eab87eb8071fbf62ce0b2c4692366c49
sou1218 <0un324410090r7v@ezweb.ne.jp>
2021-08-04 00:18:43 +0900

    テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates :family_name,      presence: true
   validates :family_name_kana, presence: true
   validates :first_name,       presence: true
-  validates :family_name_kana, presence: true
-
+  validates :first_name_kana,  presence: true
+  validates :birthday,         presence: true
   
 end
