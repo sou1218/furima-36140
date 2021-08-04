d99027ec368682962c0d85f645c86716f56a6c27
sou1218 <0un324410090r7v@ezweb.ne.jp>
2021-08-04 21:22:46 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname         
-    validates :family_name,                        format: {with:/\A[ぁ-んァ-ン一-龥]/}
+    validates :family_name,                        format: {with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
     validates :family_name_kana,                   format: {with:/\A[ァ-ヶー－]+\z/}
-    validates :first_name,                         format: {with:/\A[ぁ-んァ-ン一-龥]/}
+    validates :first_name,                         format: {with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
     validates :first_name_kana,                    format: {with:/\A[ァ-ヶー－]+\z/}
     validates :birthday         
     validates :password,                           format: {with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
