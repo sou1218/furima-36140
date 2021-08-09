2f4e886d53c3f9e835ba0fc2b7fc7a73c0f01855
sou1218 <0un324410090r7v@ezweb.ne.jp>
2021-08-09 11:20:10 +0900

    ActiceHash導入

--- app/models/buy_item.rb
+++ app/models/buy_item.rb
@@ -1,4 +1,31 @@
 class BuyItem < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :condition_id
+  belongs_to :delivery_fee_id
+  belongs_to :delivery_day_id
+  belongs_to :prefecture_id
+  belongs_to :category_id
+
+  with_options numericality: { other_than: 1, message: "can't be blanck"} do
+    validates :condition_id
+    validates :delivery_day_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :category_id
+  end
+
+  with_options presence: true do
+    validates :name
+    validates :price
+    validates :experience
+    validates :condition_id
+    validates :prefecture_id
+    validates :category_id
+    validates :delivery_day_id
+    validates :delivery_fee_id
+  end
+
 end
---
+++ app/models/category.rb
@@ -0,0 +1,17 @@
+class Category < ActiveHash::Base
+  self.data = [
+    {id:1, name: '---'},
+    {id:2, name: 'メンズ、レデイース'},
+    {id:3, name: 'ベビー・キッズ'},
+    {id:4, name: 'インテリア・住まい・小物'},
+    {id:5, name: '本・音楽・ゲーム'},
+    {id:6, name: 'おもちゃ・ホビー・グッズ'},
+    {id:7, name: '電話・スマホ・カメラ'},
+    {id:8, name: 'スポーツ・レジャー'},
+    {id:9, name: 'ハンドメイド'},
+    {id:10, name: 'その他'}
+  ]
+
+  include ActiveHash::Associations
+  has_many :buy_items
+end
