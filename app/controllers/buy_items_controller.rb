642068cd902534fb8cd366de2f867c743efde5d2
sou1218 <0un324410090r7v@ezweb.ne.jp>
2021-08-17 20:14:47 +0900

    修正

--- app/controllers/buy_items_controller.rb
+++ app/controllers/buy_items_controller.rb
@@ -1,6 +1,7 @@
 class BuyItemsController < ApplicationController
 
   before_action :set_message, only: [:create, :new]
+  before_action :second_message, only: [:show, :edit, :update]
 
   def index
     @buy_items = BuyItem.order(id: "DESC")
@@ -20,18 +21,15 @@ class BuyItemsController < ApplicationController
   end
 
   def show
-    @buy_item = BuyItem.find(params[:id])
   end
 
   def edit
-    @buy_item = BuyItem.find(params[:id])
     unless @buy_item.user == current_user
       redirect_to root_path
     end
   end
 
   def update
-    @buy_item = BuyItem.find(params[:id])
     if @buy_item.update(buy_item_params)
       redirect_to buy_item_path
     else
@@ -43,12 +41,16 @@ class BuyItemsController < ApplicationController
   private
 
   def buy_item_params
-    params.require(:buy_item).permit(:image, :name, :price, :experience, :condition_id,  :delivery_fee_id, :delivery_day_id, :category_id, :prefecture_id).merge(user_id: current_user.id)
+    params.require(:buy_item).permit(:image, :name, :price, :experience, :condition_id, :delivery_fee_id, :delivery_day_id, :category_id, :prefecture_id).merge(user_id: current_user.id)
   end
 
   def set_message
     unless user_signed_in?
       redirect_to new_user_session_path
     end
+  end
+
+  def second_message
+    @buy_item = BuyItem.find(params[:id])
   end
 end
